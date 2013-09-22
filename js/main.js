b3722c5699c48a8d43f7cac603af65f658aab01e
Kermie5000 <slandry11@gmail.com>
2013-09-22 00:49:44 -0400

    Trying to fix problem where can't refresh list after transitions

--- js/main.js
+++ js/main.js
@@ -22,12 +22,13 @@ var app = {
         var currentPageDest,
             self = this;     // If there is no current page (app just started) -> No transition: Position new page in the view port
             if (!this.currentPage) {
-                $(page.el).attr('class', 'page stage-center');
+                $(page.el).attr('class', 'page stage-center')
+                $(page.el).attr('id', 'homePage'); // add ID !
                 $('body').append(page.el);
                 this.currentPage = page;
                 return;
             }     // Cleaning up: remove old pages that were moved out of the viewport
-            $('.stage-right, .stage-left').not('.homePage').remove();
+            $('.stage-right, .stage-left').not('#homePage').remove();
         if (page === app.homePage) {        // Always apply a Back transition (slide from left) when we go back to the search page
          $(page.el).attr('class', 'page stage-left');
             currentPageDest = "stage-right";
@@ -37,6 +38,7 @@ var app = {
             currentPageDest = "stage-left";
         }
         $('body').append(page.el);     // Wait until the new page has been added to the DOM...
+
         setTimeout(function() {        // Slide out the current page: If new page slides from the right -> slide current page to the left, and vice versa
         $(self.currentPage.el).attr('class', 'page transition ' + currentPageDest);        // Slide in the new page
             $(page.el).attr('class', 'page stage-center transition');
